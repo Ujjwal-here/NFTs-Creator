9398f436c6766ab3b9566de72afbcbd72e40d097
Ujjwal-here <ujjwalsbx@gmail.com>
2022-04-04 17:44:10 +0530

    logic for multiple layers

--- components/Layers.js
+++ components/Layers.js
@@ -4,35 +4,63 @@ import Image from "next/image";
 import block from "../public/block-chain.png";
 
 const Layers = () => {
-  const [layer, setlayer] = useState("Add Layer");
+
   const [images, setImages] = useState([]);
+  const [layers,setlayers]=useState([])
 
   const onImageChange = (event) => {
-    let files = event.target.files
-    let img = []
-    for(var i=0;i<files.length;i++){
-      img.push(files[i])
+    let files = event.target.files;
+    let img = [];
+    for (var i = 0; i < files.length; i++) {
+      img.push(files[i]);
     }
-    setImages(img)
+    setImages(img);
   };
 
   const handleClick = () => {
     const newlayer = prompt("Enter Layer Name");
-    setlayer(newlayer);
+    setlayers([...layers,newlayer])
+    console.log(layers);
   };
   return (
-    <div className="flex flex-row px-40 py-6">
-      <div className="px-20 py-4 rounded-md bg-[#1C2025] text-start cursor-pointer flex flex-row items-center">
-        <p onClick={handleClick} className="text-white bg-[#1C2025]">
-          {layer}
-        </p>
-        <input accept="image/*" type="file" multiple onChange={onImageChange} />
+    <div className="flex flex-row items-stretch px-40 py-6">
+      <div className="flex flex-col">
+        {layers.map((item, i) => {
+          <div
+            key={i}
+            className="px-20 py-4 rounded-md self-start bg-[#1C2025] text-start cursor-pointer flex flex-row items-center"
+          >
+            <p className="text-white bg-[#1C2025]">{item}</p>
+            <input
+              accept="image/*"
+              type="file"
+              multiple
+              onChange={onImageChange}
+            />
+          </div>;
+        })}
+        <div className="px-20 py-4 rounded-md self-start bg-[#1C2025] text-start cursor-pointer flex flex-row items-center">
+          <p onClick={handleClick} className="text-white bg-[#1C2025]">
+            Add layers
+          </p>
+          <input
+            accept="image/*"
+            type="file"
+            multiple
+            onChange={onImageChange}
+          />
+        </div>
       </div>
       <div className="flex flex-row flex-wrap">
-          {images.map((item,i)=>(
-              <Image key={i} height={200} width={200} src={URL.createObjectURL(item)}/>
-          )
-          )}  
+        {images.map((item, i) => (
+          <Image
+            key={i}
+            height={200}
+            width={200}
+            src={URL.createObjectURL(item)}
+            alt=""
+          />
+        ))}
       </div>
     </div>
   );
