87af4c381e5322f66bb8af07d65148d7061e6c00
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2022-04-04 15:12:53 +0530

    add multiple images

--- components/Layers.js
+++ components/Layers.js
@@ -8,8 +8,12 @@ const Layers = () => {
   const [images, setImages] = useState([]);
 
   const onImageChange = (event) => {
-    console.log(images);
-    setImages(event.target.files);
+    let files = event.target.files
+    let img = []
+    for(var i=0;i<files.length;i++){
+      img.push(files[i])
+    }
+    setImages(img)
   };
 
   const handleClick = () => {
@@ -17,7 +21,7 @@ const Layers = () => {
     setlayer(newlayer);
   };
   return (
-    <div className="px-40 py-6 flex flex-row">
+    <div className="flex flex-row px-40 py-6">
       <div className="px-20 py-4 rounded-md bg-[#1C2025] text-start cursor-pointer flex flex-row items-center">
         <p onClick={handleClick} className="text-white bg-[#1C2025]">
           {layer}
