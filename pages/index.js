e5deecb94e198e621e71aa46b7551a43bd101d26
Ujjwal-here <ujjwalsbx@gmail.com>
2022-04-06 12:38:47 +0530

    Add footer screen

--- pages/index.js
+++ pages/index.js
@@ -2,12 +2,14 @@
 import Navbar from "../components/Navbar";
 import Main from "../components/Main"
 import Layers from "../components/Layers";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
-      <Layers/>
+      <Main/>
+      <Footer/>
     </div>
   );
 }
