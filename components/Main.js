dc724e383cabb762b0b325f9b6ee9aa5da0ff20b
Ujjwal-here <ujjwalsbx@gmail.com>
2022-04-06 12:20:00 +0530

    Add animation and minor ui changes

--- components/Main.js
+++ components/Main.js
@@ -1,23 +1,22 @@
 import React from "react";
 import Image from "next/image";
 import block from "../public/block-chain.png";
+import {BsArrowRightCircle} from 'react-icons/bs'
 
 const Main = () => {
   return (
-    <div className="flex flex-row items-stretch px-40 py-28">
+    <div className="flex flex-row items-stretch  px-40 py-28">
       <div className="flex-1 flex-col px-16">
         <p className="text-white font-bold text-7xl">NFTs Creator</p>
         <p className="text-[#43F2AC] font-bold text-5xl py-2">
           Generate Rare Digital ArtWork of NFTs
         </p>
-        <p className="text-[#64748b] font-thin text-md py-2">
+        <p className="text-[#cbd5e1] font-thin text-lg py-4">
           Easily create amazing NFT artworks in minutes with the Fotor-NFT
           Creator. Empower your unique digital arts with photos with crypto and
           become a crypto artist.
         </p>
-        <div className="bg-[#1C2025] py-6 my-6 rounded px-6" >
-            <p className="bg-[#1C2025] text-[#43F2AC]">Get Started</p>
-        </div>
+        <BsArrowRightCircle size={80} className="text-[#43F2AC] my-4 animate-pulse"/>
       </div>
       <div>
         <Image src={block} className="bg-[#1C2025]" alt="" />
