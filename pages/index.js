56c0b5ed0b6745a863112550fcfad8d3fc7f3476
Ujjwal-here <ujjwalsbx@gmail.com>
2022-04-06 16:17:43 +0530

    Add banner page

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
-
 import Navbar from "../components/Navbar";
 import Main from "../components/Main"
-import Layers from "../components/Layers";
 import Footer from "../components/Footer";
+import Banner from "../components/Banner";
 
 export default function Home() {
   return (
     <div>
       <Navbar />
       <Main/>
+      <Banner/>
       <Footer/>
     </div>
   );
